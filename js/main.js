b7a66dba82a751c5a258a4dd6ca25cc69f327468
Eric Yead <101576868+questerson@users.noreply.github.com>
2022-04-28 00:57:41 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,98 @@
+
+// Improve the example RPG to add character inventory management according to the following rules:
+
+// A character's inventory contains a number of gold and a number of keys.
+
+// Each character begins with 10 gold and 1 key.
+
+// The character description must show the inventory state.
+
+// When a character slays another character, the victim's inventory goes to its vanquisher.
+
+// Here's the expected execution result.
+
+
+
+
+
+
+class Character {
+  constructor(name, health, strength, inventory, gold, key) {
+    this.name = name;
+    this.health = health;
+    this.strength = strength;
+    this.xp = 0; // XP is always zero for new characters
+    this.gold = 10
+    this.key = 1
+  }
+  // Attack a target
+  attack(target) {
+    if (this.health > 0) {
+      const damage = this.strength;
+      console.log(
+        `${this.name} attacks ${target.name} and causes ${damage} damage points`
+      );
+      target.health -= damage;
+      if (target.health > 0) {
+        console.log(`${target.name} has ${target.health} health points left`);
+      } else {
+        target.health = 0;
+        const bonusXP = 10;
+        
+        console.log(
+          `${this.name} eliminated ${target.name} and wins ${bonusXP} experience points, ${target.gold} gold, and ${target.key} key`
+        );
+        this.xp += bonusXP;
+        this.gold += target.gold
+        this.key += target.key
+      }
+    } else {
+       
+      console.log(`${this.name} can't attack (they've been eliminated)`);
+    }
+  }
+  // Return the character description
+  describe() {
+    return `${this.name} has ${this.health} health points, ${this
+      .strength} as strength, ${this.gold} gold, ${this.key} keys and ${this.xp} XP points`;
+  }
+}
+
+const aurora = new Character("Aurora", 150, 25);
+const glacius = new Character("Glacius", 130, 30);
+
+console.log("Welcome to the adventure! Here are our heroes:");
+console.log(aurora.describe());
+console.log(glacius.describe());
+
+const monster = new Character("Spike", 40, 20);
+console.log("A wild monster has appeared: it's named " + monster.name);
+
+monster.attack(aurora);
+monster.attack(glacius);
+aurora.attack(monster);
+glacius.attack(monster);
+
+console.log(aurora.describe());
+console.log(glacius.describe());
+
+// TODO: define the Dog class here
+class Dog {
+  constructor (name, species, size) {
+    this.name = name
+    this.species = species 
+    this.size = size
+  }
+   bark() {
+     return (this.size > 60) ? 'Grrr! Grrr!' : 'woof! woof!'
+   }
+}
+const fang = new Dog("Fang", "boarhound", 75);
+console.log(`${fang.name} is a ${fang.species} dog measuring ${fang.size}`);
+console.log(`Look, a cat! ${fang.name} barks: ${fang.bark()}`);
+
+const snowy = new Dog("Snowy", "terrier", 22);
+console.log(`${snowy.name} is a ${snowy.species} dog measuring ${snowy.size}`);
+console.log(`Look, a cat! ${snowy.name} barks: ${snowy.bark()}`);
+
+
